4cc09d137ccc924e4b667799926c90f90ae1c35e
Anush B M <anushbm7@gmail.com>
2017-07-11 14:09:15 +0530

    Initial Check in.
    1) Code Base
    2) Read me
    3) Schema Diagram

---
+++ ReadMe.txt
@@ -0,0 +1,29 @@
+Date: 11th July, 2017
+Name: Weather Sensor
+Source: http://embedded-lab.com/blog/making-simple-weather-web-server-using-esp8266-bme280/
+Schema Diagram: SetUpserver.jpg
+
+Hardware: ESP8266, BME280, Connecting Wires
+Studio: Sketch
+
+Descrption:
+Task 1:
+*) To weather data from ESP8266 by connection BME280.
+*) Default Port to access live data: "ESP-IP"
+ 
+Task 2:
+*) Passing data using Rest Web Services
+ 
+Task 3:
+*) Sending data to ELK Stack deployed on Amazon Cloud Instance.
+   Amazon Instance: "IP"
+
+Wifi Connection:
+SSID: Airtel-WD670-0857   
+   
+   
+Objective:
+To read data for over a month using the ESP8266 and displaying it using ELK Stack deployed on Amazon service.
+
+	
+
---
+++ bme280_Rest/bme280_Rest.ino
@@ -0,0 +1,168 @@
+#include <Wire.h>
+#include <ESP8266WiFi.h>
+#include <ArduinoJson.h>
+#include <ESP8266HTTPClient.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+
+Adafruit_BME280 bme; // I2C
+
+// Replace with your network details
+const char* ssid = "Airtel-WD670-0857";
+const char* password = "D8F20857";
+float h, t, p, pin, dp, dc;
+char temperatureFString[6];
+char dpString[6];
+char dcString[6];
+char humidityString[6];
+char pressureString[7];
+char pressureInchString[6];
+
+// Web Server on port 80
+WiFiServer server(80);
+
+// only runs once on boot
+void setup() {
+  // Initializing serial port for debugging purposes
+  Serial.begin(115200);
+  delay(10);
+  Wire.begin(D3, D4);
+  Wire.setClock(100000);
+  // Connecting to WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  
+  // Starting the web server
+  server.begin();
+  Serial.println("Web server running. Waiting for the ESP IP...");
+  delay(10000);
+  
+  // Printing the ESP IP address
+  Serial.println(WiFi.localIP());
+  Serial.println(F("BME280 test"));
+
+  if (!bme.begin()) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1);
+  }
+}
+
+void getWeather() {
+  
+    h = bme.readHumidity();
+    t = bme.readTemperature();
+    t = t*1.8+32.0;
+    dp = t-0.36*(100.0-h);
+    dc = (t - 32) * 0.55;
+    
+    p = bme.readPressure()/100.0F;
+    pin = 0.02953*p;
+    dtostrf(t, 5, 1, temperatureFString);
+    dtostrf(h, 5, 1, humidityString);
+    dtostrf(p, 6, 1, pressureString);
+    dtostrf(pin, 5, 2, pressureInchString);
+    dtostrf(dp, 5, 1, dpString);
+    dtostrf(dc, 5, 1, dcString);
+    delay(100);
+ 
+}
+
+// runs over and over again
+void loop() {
+  // Listenning for new clients
+  WiFiClient client = server.available();
+  
+  if (client) {
+    Serial.println("New client");
+    // bolean to locate when the http request ends
+    boolean blank_line = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        
+        if (c == '\n' && blank_line) {
+            getWeather();
+            //sendDataPost();
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-Type: text/html");
+            client.println("Connection: close");
+            client.println();
+            // your actual web page that displays temperature
+            client.println("<!DOCTYPE HTML>");
+            client.println("<html>");
+            client.println("<head><META HTTP-EQUIV=\"refresh\" CONTENT=\"15\"></head>");
+            client.println("<body><h1>ESP8266 Weather Web Server</h1>");
+            client.println("<table border=\"2\" width=\"456\" cellpadding=\"10\"><tbody><tr><td>");
+            client.println("<h3>Temperature in Fahrenheit = ");
+            client.println(temperatureFString);
+            client.println("&deg;F</h3><h3>Temperature in Celcius = ");
+            client.println(dcString);
+            client.println("&deg;C</h3><h3>Humidity = ");
+            client.println(humidityString);
+            client.println("%</h3><h3>Approx. Dew Point = ");
+            client.println(dpString);
+            client.println("&deg;F</h3><h3>Pressure = ");
+            client.println(pressureString);
+            client.println("hPa (");
+            client.println(pressureInchString);
+            client.println("Inch)</h3></td></tr></tbody></table></body></html>");
+            
+            break;
+        }
+        if (c == '\n') {
+          // when starts reading a new line
+          blank_line = true;
+        }
+        else if (c != '\r') {
+          // when finds a character on the current line
+          blank_line = false;
+        }
+      }
+    }  
+    // closing the client connection
+    delay(1);
+    client.stop();
+    Serial.println("Client disconnected.");
+  }
+}
+
+void sendDataPost() 
+{
+    if(WiFi.status()== WL_CONNECTED){   //Check WiFi connection status
+        HTTPClient http;    //Declare object of class HTTPClient
+        http.begin("http://192.168.1.15:8080/api");      //Specify request destination
+        http.addHeader("Content-Type", "application/json");    //Specify content-type header
+
+        StaticJsonBuffer<200> jsonBuffer;
+        JsonObject& root = jsonBuffer.createObject();
+        root["sensor"] = "bme280";
+        root["time"] = 1351824120;
+
+        JsonArray& data = root.createNestedArray("data");
+        data.add(temperatureFString);
+        data.add(humidityString);
+
+        String output;
+        root.printTo(output);
+        
+        int httpCode = http.POST(output);   //Send the request
+        String payload = http.getString();  //Get the response payload
+        Serial.println(httpCode);   //Print HTTP return code
+        Serial.println(payload);    //Print request response payload
+        http.end();  //Close connection
+    }else{
+        Serial.println("Error in WiFi connection");   
+    }
+}
+
